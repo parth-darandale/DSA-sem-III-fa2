9b1c0218ed5b4b1f92451e3daca0be4e7f66c668
parth-darandale <parthdarandale5@gmail.com>
2024-10-26 09:07:14 +0530

    Add files via upload

--- stack.h
+++ stack.h
@@ -8,15 +8,14 @@ PRN no.- 123B1B040*/
 using namespace std;
 
 template <typename T>
-
-class Node{
-    private:
+class Node {
+private:
     T data;
-    Node * next;
-    Node * prev;
+    Node *next;
+    Node *prev;
 
-    public:
-    Node(T v){
+public:
+    Node(T v) {
         data = v;
         next = NULL;
         prev = NULL;
@@ -27,69 +26,69 @@ class Node{
 };
 
 template <typename T>
-
 class Stack {
-    private:
-        Node<T> *tail;
+private:
+    Node<T> *tail;
+    int count;
 
-    public:
-        Stack() {
+public:
+    Stack() {
+        tail = NULL;
+        count = 0;
+    }
+
+    bool empty() {
+        return tail == NULL;
+    }
+
+    void push(T end) {
+        Node<T> *nn = new Node<T>(end);
+        if (tail == NULL) {
+            tail = nn;
+        } else {
+            nn->prev = tail;
+            tail->next = nn;
+            tail = nn;
+        }
+        count++;
+    }
+
+    T pop() {
+        if (tail == NULL) {
+            cout << "Empty Stack" << endl;
+            return 0;
+        }
+
+        T n = tail->data;
+        Node<T> *temp = tail;
+        if (!(tail->prev)) {
             tail = NULL;
+        } else {
+            tail = tail->prev;
+            tail->next = NULL;
         }
+        delete temp;
+        count--;
+        return n;
+    }
 
-        void push(T end) {
-            Node<T> *nn = new Node<T>(end);
-            if (tail == NULL) {
-                tail = nn;
-            } 
-            
-            else {
-                nn->prev = tail;
-                tail->next = nn;
-                tail = nn;
-            }
+    T peek() {
+        if (tail == NULL) {
+            cout << "Empty Stack" << endl;
+            return 0;
         }
+        return tail->data;
+    }
 
-        T pop(){
-            if (tail == NULL){
-                cout<< "Empty Stack" << endl;
-                return 0;
-            }
-
-
-            T n = tail->data;
-
-            Node<T> * temp = tail;
-            if (!(tail->prev)){
-                tail = NULL;
-            }
-            else{
-                tail = tail->prev;
-                tail->next = NULL;
-            }
-
-            delete temp;
-
-            return n;
+    void display() {
+        Node<T> *temp = tail;
+        while (temp) {
+            cout << temp->data << " ";
+            temp = temp->prev;
         }
+    }
 
-        T peek(){
-            if (tail == NULL){
-                cout<< "Empty Stack" << endl;
-                return 0;
-            }
-
-            return tail->data;
-        }
-
-        void display(){
-            Node<T> *temp = tail;
-            while(temp){
-                cout << temp->data << " ";
-                temp = temp->prev;
-
-            }
-
-            return;
-        }
+    int size() {
+        return count;
+    }
 };
